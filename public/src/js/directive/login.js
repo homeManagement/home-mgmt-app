110eb88b02f266de8dd6b03a20366391877e6279
Ben Schinn <schinn.ben@gmail.com>
2016-10-25 11:22:31 -0600

    authorizing through sateliizer JWT facebook

--- public/src/js/directive/login.js
+++ public/src/js/directive/login.js
@@ -2,12 +2,30 @@ angular.module('homeMgmt').directive('loginDirective', function(){
 
 var controller = function($scope, $auth){
   $scope.authenticate = function(provider) {
+    // localStorage.clear();
     $auth.authenticate(provider)
     .then(function(response){
       console.log(response);
     })
   };
+
+  $scope.emailLogin = function() {
+       $auth.login({ email: $scope.email, password: $scope.password })
+         .then(function(response) {
+           $window.localStorage.currentUser = JSON.stringify(response.data.user);
+           $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+         })
+         .catch(function(response) {
+           $scope.errorMessage = {};
+           angular.forEach(response.data.message, function(message, field) {
+             $scope.loginForm[field].$setValidity('server', false);
+             $scope.errorMessage[field] = response.data.message[field];
+           });
+         });
+     };
 }
+
+
 
 return{
   restrict: 'AE',
--- server.js
+++ server.js
@@ -62,6 +62,37 @@ function createJWT(user) {
 
 /*
  |--------------------------------------------------------------------------
+ | Log in with Email
+ |--------------------------------------------------------------------------
+ */
+app.post('/auth/login', function(req, res) {
+  console.log(req.body);
+  db.getLocalUser([req.body.email], function(err, user){
+    console.log('xyxyxyxyxyx',user);
+    if (!user){
+      return res.status(401).send({message: 'Invalid email and/or password'});
+    }
+    if (req.body.password !== user[0].password){
+      return res.status(401).send({message: 'Invalid email and/or password'});
+    }
+    res.send({ token: createJWT(user[0]) });
+  })
+  // User.findOne({ email: req.body.email }, '+password', function(err, user) {
+  //   if (!user) {
+  //     return res.status(401).send({ message: 'Invalid email and/or password' });
+  //   }
+  //   user.comparePassword(req.body.password, function(err, isMatch) {
+  //     if (!isMatch) {
+  //       return res.status(401).send({ message: 'Invalid email and/or password' });
+  //     }
+  //     res.send({ token: createJWT(user) });
+  //   });
+  // });
+});
+
+
+/*
+ |--------------------------------------------------------------------------
  | Login with Facebook
  |--------------------------------------------------------------------------
  */
@@ -76,13 +107,11 @@ function createJWT(user) {
      client_secret: config.FACEBOOK_SECRET,
      redirect_uri: req.body.redirectUri
    };
-   console.log(params)
    // Step 1. Exchange authorization code for access token.
    request.get({ url: accessTokenUrl, qs: params, json: true }, function(err, response, accessToken) {
      if (response.statusCode !== 200) {
        return res.status(500).send({ message: accessToken.error.message });
      }
-
      // Step 2. Retrieve profile information about the current user.
      request.get({ url: graphApiUrl, qs: accessToken, json: true }, function(err, response, profile) {
        if (response.statusCode !== 200) {
@@ -95,9 +124,10 @@ function createJWT(user) {
              return res.status(409).send({ message: 'There is already a Facebook account that belongs to you' });
            }
            var token = req.header('Authorization').split(' ')[0];
-           console.log(token, config.TOKEN_SECRET);
+
+          //  console.log(token, config.TOKEN_SECRET);
            var payload = jwt.decode(token, config.TOKEN_SECRET);
-          //  console.log(payload);
+           console.log(payload);
           //  User.findById(payload.sub, function(err, user) {
           //    if (!user) {
           //      return res.status(400).send({ message: 'User not found' });
@@ -112,22 +142,20 @@ function createJWT(user) {
           //  });
          });
        } else {
-         console.log('LLLLLLLLŁ')
+         console.log('LLLLLLLLŁ',profile)
          // Step 3. Create a new user account or return an existing one.
-        //  User.findOne({ facebook: profile.id }, function(err, existingUser) {
-        //    if (existingUser) {
-        //      var token = createJWT(existingUser);
-        //      return res.send({ token: token });
-        //    }
-        //    var user = new User();
-        //    user.facebook = profile.id;
-        //    user.picture = 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
-        //    user.displayName = profile.name;
-        //    user.save(function() {
-        //      var token = createJWT(user);
-        //      res.send({ token: token });
-        //    });
-        //  });
+         db.getuser([profile.id], function(err, existingUser) {
+           if (existingUser[0]) {
+             var token = createJWT(existingUser);
+             return res.send({ token: token });
+           }
+           db.createuser([profile.id, profile.first_name, profile.last_name, profile.email,'https://graph.facebook.com/' + profile.id + '/picture?type=large'], function(err,success){
+             db.getuser([profile.id], function(err, existingUser) {
+                 var token = createJWT(existingUser);
+                 return res.send({ token: token });
+               });
+           });
+         });
        }
      });
    });
