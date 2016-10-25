c8e6fa8f45ebf9c95a9cb5b8d1bf21f66ea3d89b
JustinBillings <billings.justinm@gmail.com>
2016-10-25 12:16:13 -0600

    added config.json and fixed conflict of appName

--- public/src/js/directive/login.js
+++ public/src/js/directive/login.js
@@ -1,4 +1,4 @@
-angular.module('homeMgmt').directive('loginDirective', function(){
+angular.module('mgmtApp').directive('loginDirective', function(){
 
 var controller = function($scope, $auth){
   $scope.authenticate = function(provider) {
