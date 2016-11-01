6eb42cd7e3a22da81a3fd0f7c84a09e3085a5d5c
Daniel Gardner <bowshanks@gmail.com>
2016-11-01 15:30:51 -0600

    beginning of delete property

--- public/src/js/controllers/propertiesCtrl.js
+++ public/src/js/controllers/propertiesCtrl.js
@@ -1,12 +1,23 @@
 angular.module('mgmtApp')
-.controller('propertiesCtrl', function($scope, $window, mainService){
+.controller('propertiesCtrl', function($scope, $window, mainService, $swipe){
 
-$scope.getProperties = function(token){
-  mainService.getProperties(token)
-  .then(function(res){
-    $scope.properties = res;
-  })
-}
-$scope.getProperties($window.localStorage.satellizer_token)
+  $scope.propDeleteButtonVisibility = false;
+  $scope.propDeleteConfirmVisiblity = false;
+
+  $scope.getProperties = function(token){
+    mainService.getProperties(token)
+    .then(function(res){
+      $scope.properties = res;
+    })
+  }
+  $scope.getProperties($window.localStorage.satellizer_token)
+
+  $scope.deleteProperty = function(propertyId){
+    mainService.deleteProperty(propertyId).then(function(res){
+      $scope.properties = $scope.properties.filter(function(currentProp){
+        return currentProp.id !== propertyId
+      });
+    })
+  }
 
 })
